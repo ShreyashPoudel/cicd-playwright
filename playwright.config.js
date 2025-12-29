12d0f790d9e2b6f46b30435154d72e35b7391288
Shreyash Poudel <poudelshreyash36@gmail.com>
2025-12-29 13:54:20 +0545

    changed headless false to true

--- playwright.config.js
+++ playwright.config.js
@@ -15,7 +15,7 @@ export default defineConfig({
   reporter: [['html'], ['github']],
 
   use: {
-    headless: false,
+    headless: true,
     screenshot: "only-on-failure",
     video: "retain-on-failure",
     trace: 'on-first-retry',
